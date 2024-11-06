20b793c7353a4409aa6eb57254b665cc57e0ebe5
Leonid <leonigleo13@gmail.com>
2024-11-06 13:13:56 +0300

    Fifth step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,15 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
@@ -20,11 +26,24 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        System.out.print("Hello world!");
+        try {
+            Map<String, Object> data1 = getData(filepath1);
+            Map<String, Object> data2 = getData(filepath2);
+
+            System.out.println(data1);
+            System.out.println(data2);
+        } catch (Exception e) {
+            System.err.println("Error reading or parsing files: " + e.getMessage());
+        }
     }
 
-    public static void main(String[] args) {
+    public static Map<String, Object> getData(String filepath) throws Exception {
+        ObjectMapper result = new ObjectMapper();
+        return result.readValue(new File(filepath), new TypeReference<Map<String, Object>>() {});
+    }
+    public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+
     }
 }
