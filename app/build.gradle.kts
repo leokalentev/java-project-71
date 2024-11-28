d324f1e14ca57ce7a74e4a5d1eb65fb03088a4be
Leonid <leonigleo13@gmail.com>
2024-11-28 18:03:36 +0300

    Ninth step #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
 }
 
 
--- app/src/main/java/hexlet/code/GetData.java
+++ app/src/main/java/hexlet/code/GetData.java
@@ -2,13 +2,20 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.util.Map;
 
 public class GetData {
     public static Map<String, Object> getData(String filepath) throws Exception {
-        ObjectMapper result = new ObjectMapper();
-        return result.readValue(new File(filepath), new TypeReference<Map<String, Object>>() { });
+        ObjectMapper objectMapper;
+        if (filepath.endsWith(".yml") || filepath.endsWith(".yaml")) {
+            objectMapper = new ObjectMapper(new YAMLFactory());
+        } else {
+            objectMapper = new ObjectMapper();
+        }
+
+        return objectMapper.readValue(new File(filepath), new TypeReference<Map<String, Object>>() { });
     }
 }
